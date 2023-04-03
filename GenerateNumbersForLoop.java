59f41ea762cc9c72fb0a275335a331222baa04b3
MILKA-WANGA <83470011+MILKA-WANGA@users.noreply.github.com>
2023-04-03 02:07:28 -0700

    initial commit

---
+++ GenerateNumbersForLoop.java
@@ -0,0 +1,13 @@
+public class GenerateNumbersForLoop
+{
+    public static void main(String[]args)
+    {  
+
+    for(int i=1; i<=20; i++)
+
+    {
+      System.out.println(i);
+    }
+    
+    }
+}
